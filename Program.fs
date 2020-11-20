bd3a445ef5a2174a2f31c70bb6ac132b4d64b47c
Constantin Tews <constantin.tews@gmail.com>
2020-11-20 22:14:19 +0100

    Prepare entry point for more commands.

--- Program.fs
+++ Program.fs
@@ -4,6 +4,14 @@ open System
 
 module Program =
 
+    let findCommands (args: string[]) =
+        match args.[0] with
+        | "-k" | "--set-key" -> Commands.setKey args.[1]
+        | "-a" | "--api-key" -> Commands.getKey |> Console.WriteLine
+        | "-t" | "--text" -> Commands.pushText args.[1]
+        /// Add more commands.
+        | _ -> Console.WriteLine("Command not found!")
+
     [<EntryPoint>]
     let main ([<ParamArray>] argv: string[]): int =
         let command = argv.[0]
@@ -11,11 +19,7 @@ module Program =
         let breakup = Commands.getKey = "" && command <> "--set-key"
 
         if not breakup then
-            match command with
-            | "-k" | "--set-key" -> Commands.setKey argv.[1]
-            | "-t" | "--text" -> Commands.pushText argv.[1]
-            /// Add more commands.
-            | _ -> Console.WriteLine("Command not found!")
+            findCommands argv
         else
             Console.WriteLine("You have to set your API key with: \"--set-key o.Abc12345xyz\" ")
 
